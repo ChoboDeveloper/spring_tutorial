87d806b352ee4b80030289963b39ccf4fc4616fc
ChoboDeveloper <howtliv@gmail.com>
2021-03-29 20:06:05 +0900

    first commit

---
+++ src/main/java/com/springtutorial/demo/Taco.java
@@ -0,0 +1,10 @@
+package com.springtutorial.demo;
+
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class Taco {
+    private String name;
+    private List<String> ingredients;
+}
---
+++ src/test/java/com/springtutorial/demo/DemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.springtutorial.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
